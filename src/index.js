b7a19a0c25c0c475b28ce247e1f991bbbca89495
Chaitanya biswas <chaitanya.biswas.22@gmail.com>
2022-09-24 18:32:13 +0530

    kar do

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Mlc from "./Mlc";
+import App from "./Mlc";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Mlc />
+    <App />
   </React.StrictMode>
 );
 
